bb0ccd9e5ff69c06af40ff7351465438313d47f9
Oliver Matelowsi <olivermatelowski@yahoo.de>
2018-12-28 17:56:32 +0100

    new

---
+++ Sketch Document/viewer/story.js
@@ -0,0 +1,55 @@
+var story = {
+"docName": "sketch-document",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"hasRetina": true,
+"disableHotspots": false,
+"pages": [
+$.extend(new ViewerPage(),{
+"index": 0,
+"image": "test1.png",
+"image2x": "test1@2x.png",
+"width": 375,
+"height": 667,
+"title": "test1",
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 136,
+			"y": 417,
+			"width": 104,
+			"height": 104
+		},
+		"page": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 1,
+"image": "test1_copy.png",
+"image2x": "test1_copy@2x.png",
+"width": 375,
+"height": 667,
+"title": "test1 copy",
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 114.46089475659903,
+			"y": 395.46089475659903,
+			"width": 147.07821048680205,
+			"height": 147.07821048680194
+		},
+		"page": 0
+	}
+],
+})
+   ]
+,"resolutions": [2],
+"title": "Sketch Document",
+"totalImages": 2,
+"highlightLinks": false
+}
