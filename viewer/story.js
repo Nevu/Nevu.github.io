c35eff446dcd1d9848db6210ed982ff979fa9bba
Oliver Matelowsi <olivermatelowski@yahoo.de>
2018-12-28 18:30:22 +0100

    new

---
+++ viewer/story.js
@@ -0,0 +1,47 @@
+var story = {
+"docName": "sketch-document",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"hasRetina": true,
+"disableHotspots": false,
+"pages": [
+$.extend(new ViewerPage(),{
+"index": 0,
+"image": "iphone_8.png",
+"image2x": "iphone_8@2x.png",
+"width": 375,
+"height": 1594,
+"title": "iPhone 8",
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": true,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": true,
+			"width": true
+		},
+		"x": 0,
+		"y": 0,
+		"width": 375,
+		"height": 99,
+		"type": "top",
+		"index": 0,
+		"isFloat": false,
+		"isSplit": false,
+		"divID": "",
+		"links": [],
+		"image": "iphone_8.png",
+		"image2x": "iphone_8@2x.png"
+	}
+],
+'links' : [],
+})
+   ]
+,"resolutions": [2],
+"title": "Sketch Document",
+"totalImages": 2,
+"highlightLinks": false
+}
